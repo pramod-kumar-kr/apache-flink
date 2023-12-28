7ade958cc5fd8517d08593970dbf0d0c458350dc
pramod-kumar-kr <pkumar5810a@kroger.com>
2023-12-28 12:13:49 +0530

    Changed dependencies

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Submit Flink job
-flink run -c org.flink.Main /Users/zop7917/IdeaProjects/apache-flink/build/pipeline-1.0-SNAPSHOT.jar
+flink run -c org.flink.pipeline.ApacheFlinkPipeline /Users/zop7917/IdeaProjects/apache-flink/build/pipeline-1.0-SNAPSHOT.jar
